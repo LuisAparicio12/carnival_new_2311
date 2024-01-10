65ae582a68d7dc30aa021462f7561594f7fabbe8
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-01-10 02:04:48 -0500

    Create the carnival class and has different methods

---
+++ lib/carnival.rb
@@ -0,0 +1,23 @@
+class Carnival
+    attr_reader :duration, :rides
+    def initialize(duration)
+        @duration = duration
+        @rides = []
+    end
+
+    def add_ride(ride)
+        @rides << ride
+    end
+
+    def most_popular_ride
+        @rides.max_by {|ride| ride.rider_log.values.sum}
+    end
+
+    def most_profitable_ride
+        @rides.max_by(&:total_revenue)
+    end
+
+    def total_revenue
+        @rides.sum(&:total_revenue)
+    end
+end
