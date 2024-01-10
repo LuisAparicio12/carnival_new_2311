fffc53d3efa189005cf1ce529a723e2c4d2963b5
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-01-10 00:04:53 -0500

    Create TDD for the visitor class

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -1,2 +1,35 @@
 require 'spec_helper'
 
+RSpec.describe Visitor do
+    it 'exits' do 
+        visitor1 = Visitor.new('Bruce', 54, '$10')
+
+        expect(visitor1).to be_an_instance_of Visitor
+    end 
+
+    it 'can have attributes' do
+        visitor1 = Visitor.new('Bruce', 54, '$10')
+
+        expect(visitor1.name).to eq("Bruce")
+        expect(visitor1.height).to eq(54)
+        # visitor1.spending_money = spending_money.to_s.delete("$").to_i
+        expect(visitor1.spending_money).to eq(10)
+        expect(visitor1.preferences).to be_an Array
+
+        visitor1.add_preference(:gentle)
+        visitor1.add_preference(:thrilling)
+
+        expect(visitor1.preferences).to eq([:gentle, :thrilling])
+    end 
+
+    it 'can be tall enough' do
+        visitor1 = Visitor.new('Bruce', 54, '$10')
+        visitor2 = Visitor.new('Tucker', 36, '$5')
+        visitor3 = Visitor.new('Penny', 64, '$15')
+
+        expect(visitor1.tall_enough?(54)).to eq(true)
+        expect(visitor2.tall_enough?(54)).to eq(false)
+        expect(visitor3.tall_enough?(54)).to eq(true)
+        expect(visitor1.tall_enough?(64)).to eq(false)
+    end 
+end 
